b7dbc3784029079a257966ed2dc9c6c9e7b70517
Gideon Tong <funnygid@gmail.com>
2018-10-13 12:24:34 -0700

    I've done something wrong

--- name-tbd/App.js
+++ name-tbd/App.js
@@ -27,6 +27,7 @@ export default class App extends React.Component {
         },
         isLoading: false
       });
+      console.log(this);
     }, (error) => {
       alert(JSON.stringify(error))
     });    
