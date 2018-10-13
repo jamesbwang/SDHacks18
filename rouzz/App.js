d4aae45b36571307d3c31ecd057ba9c7527ec773
Andrew Chau <andrewchau333@gmail.com>
2018-10-13 09:27:40 -0700

    Got react navigation running

--- rouzz/App.js
+++ rouzz/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
+import { createStackNavigator, createBottomTabNavigator } from 'react-navigation';
 
-export default class App extends React.Component {
+
+export default class HomePage extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+        <Text>React navigation is working!</Text>
       </View>
     );
   }
