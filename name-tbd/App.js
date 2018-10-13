a511b9da8af203c60b88331f484256e7dbd78c1d
Gideon Tong <funnygid@gmail.com>
2018-10-13 08:54:17 -0700

    Get GPS and loading indicator
    This code reivision adds a loading indiactor and the ability to get GPS location

--- name-tbd/App.js
+++ name-tbd/App.js
@@ -1,11 +1,47 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+  StyleSheet,
+  Text,
+  View,
+  ActivityIndicator,
+} from 'react-native';
 
 export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isLoading: true
+    }
+  }
+
+  componentWillMount() {
+    return navigator.geolocation.getCurrentPosition((position) => {
+      this.setState({
+        position: {
+          longitude: position.longitude,
+          latitude: position.latitude
+        },
+        isLoading: false
+      })
+      console.log(position);
+    }, (error) => {
+      alert(JSON.stringify(error))
+    });
+  }
+
   render() {
+    if(this.state.isLoading) {
+      return (
+        <View style={{flex: 1, padding: 20}}>
+          <ActivityIndicator/>
+        </View>
+      )
+    }
+
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+        <Text>Alarm clock with HERE API!</Text>
+        <Text></Text>
       </View>
     );
   }
