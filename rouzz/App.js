b7a1dd67f2e11f168d3f24609b528524a911af47
Andrew Chau <andrewchau333@gmail.com>
2018-10-13 09:46:45 -0700

    Basic front end navigation implementation

--- rouzz/App.js
+++ rouzz/App.js
@@ -1,14 +1,58 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Button, Text, View } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
 import { createStackNavigator, createBottomTabNavigator } from 'react-navigation';
 
 
-export default class HomePage extends React.Component {
+export class HomeScreen extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>React navigation is working!</Text>
+        <Text>Homepage Screen!</Text>
+        <Button
+          title="Go to Countdown"
+          onPress={() => this.props.navigation.navigate('Countdown')}
+        />
+        <Button
+          title="Go to Alarm"
+          onPress={() => this.props.navigation.navigate('Alarm')}
+        />
+      </View>
+    );
+  }
+}
+
+export class CountdownScreen extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Countdown Screen!</Text>
+        <Button
+          title="Go to Home"
+          onPress={() => this.props.navigation.navigate('Home')}
+        />
+        <Button
+          title="Go to Alarm"
+          onPress={() => this.props.navigation.navigate('Alarm')}
+        />
+      </View>
+    );
+  }
+}
+
+export class AlarmScreen extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Alarm Screen!</Text>
+        <Button
+          title="Go to Home"
+          onPress={() => this.props.navigation.navigate('Home')}
+        />
+        <Button
+          title="Go to Countdown"
+          onPress={() => this.props.navigation.navigate('Countdown')}
+        />
       </View>
     );
   }
@@ -22,3 +66,21 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+const RootStack = createStackNavigator(
+  {
+    Home: HomeScreen,
+    Countdown: CountdownScreen,
+    Alarm: AlarmScreen
+  },
+  {
+    initialRouteName: 'Home',
+  }
+);
+
+// Starts everything 
+export default class App extends React.Component {
+  render() {
+    return <RootStack />;
+  }
+}
