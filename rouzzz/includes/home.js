67ba59ee4b6f1989c9fbb3d372f3aafd553155fa
unknown <jbwang99@ucla.edu>
2018-10-14 04:19:53 -0700

    home page edited

--- rouzzz/includes/home.js
+++ rouzzz/includes/home.js
@@ -37,24 +37,30 @@ export default class HomeScreen extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <MapView
-          ref={map => this.map = map}
-          style={styles.map}
-          initialRegion={this.state.region}
-          onMarkerDragEnd={(e)=>this.map.animateToCoordinate(e.nativeEvent.coordinate)}
-          onPress={(e) => this.updateMarker(e)}
-        >
-          <Marker draggable
-            coordinate={this.state.MarkerLatLong}
-            onDragEnd={(e) => this.updateMarker(e)}
-          />
-        </MapView>
+      <Text style={{font: 40}}>
+          How many minutes do you need to get ready?
+        </Text>
+        <TextInput
+          keyboardType='numeric'
+          underlineColorAndroid='transparent' 
+          style={{
+            height: 30,
+            width: 150,
+            borderLeftWidth: 1,
+            borderRightWidth: 1,
+            borderTopWidth: 1,
+            borderBottomWidth: 1,
+          }}
+          textAlign={'center'}
+          onChangeText={(text) => this.onChanged(text)}
+          value={this.num}
+          maxLength={3}
+        />
         <DatePicker
-        style={{width: 200}}
+        style={{width: 300}}
         showIcon={false}
-        date={this.eta}
         mode="datetime"
-        placeholder="select date"
+        placeholder="When do you have to be there?"
         is24Hour= {true}
         format="HH:mm"
         confirmBtnText="Confirm"
@@ -68,6 +74,19 @@ export default class HomeScreen extends React.Component {
           title="Let's Sleep!"
           onPress={() => this.props.navigation.navigate('Countdown')}
         />
+        <MapView
+          ref={map => this.map = map}
+          style={styles.map}
+          initialRegion={this.state.region}
+          onMarkerDragEnd={(e)=>this.map.animateToCoordinate(e.nativeEvent.coordinate)}
+          onPress={(e) => this.updateMarker(e)}
+        >
+          <Marker draggable
+            coordinate={this.state.MarkerLatLong}
+            onDragEnd={(e) => this.updateMarker(e)}
+          />
+        </MapView>
+        
         {/* <Button
           title="Go to Alarm"
           onPress={() => this.props.navigation.navigate('Alarm')}
@@ -76,16 +95,6 @@ export default class HomeScreen extends React.Component {
           title="Go to TimeFinder (Debug page)"
           onPress={() => this.props.navigation.navigate('TimeFinder')}
         /> */}
-        <Text>
-          How much time do you need to get ready?
-        </Text>
-        <TextInput
-          keyboardType='numeric'
-          defaultValue='0'
-          onChangeText={(text) => this.onChanged(text)}
-          value={this.num}
-          maxLength={3}
-        />
       </View>
     );
   }
@@ -106,13 +115,13 @@ const styles = StyleSheet.create({
     flexDirection: 'column',
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'space-around',
   },
   timeHeader: {
     fontSize: 20
   },
   map: {
-    height: 400,
+    height: 510,
     width: 500,
     justifyContent: 'flex-end',
     alignItems: 'center',
